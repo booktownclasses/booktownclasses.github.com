09548947a18075cc4057c1a6d34619671b9c5598
HuntTayyab <hollywoodhitzzz@gmail.com>
2026-05-13 23:34:47 +0530

    updated

--- js/learn-python-advance.js
+++ js/learn-python-advance.js
@@ -213,3 +213,22 @@ document.querySelectorAll('.prog-tab').forEach(tab => {
     });
   });
 });
+
+
+// Inject Google Analytics dynamically
+(function() {
+  // 1. Create the async script tag that loads gtag.js
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-FPKPM82P5B';
+  document.head.appendChild(gtagScript);
+
+  // 2. Set up the dataLayer and gtag() command queue
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  // 3. Initialise the tracker
+  gtag('js', new Date());
+  gtag('config', 'G-FPKPM82P5B');
+})();
